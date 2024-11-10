7552dad4417ba566d416fa1e441c6644e833088f
tw0ten <twoten.eq@gmail.com>
2024-11-10 22:08:07 +0200

    i

--- client/web/!/main.js
+++ client/web/!/main.js
@@ -1,39 +1,96 @@
 (async () => {
+	const e = document.getElementById("board");
 	const url = `${
-		new URLSearchParams(document.location.search).get("s") || ""
+		new URLSearchParams(document.location.search).get("s") ||
+		"http://localhost:8080"
 	}/api`;
 
-	console.log(`server: ${url}`);
-
-	const get = async (i) => {
-		const response = await fetch(`${url}/${i}`);
-		if (!response.ok) return;
-		return await response.text();
-	};
-
-	const post = async (i, h, b) => {
-		const response = await fetch(`${url}/${i}`, {
-			method: "POST",
-			headers: h,
-			body: b,
-		});
-		if (!response.ok) return;
-		return await response.text();
-	};
-
-	const game = "gametiltlte";
-
-	await import("../wasm-pack/client.js").then((m) => m.default());
-	const g = (await get(`!/${game}`)).split("\n");
-	const token = g[0];
-	const b = g[1];
-	for (let i = 0; i < Math.sqrt(b.length); i++) {
-		for (let j = 0; j < Math.sqrt(b.length); j++) {
-			document.getElementById(`${i}:${j}`).innerText =
-				b[i * Math.sqrt(b.length) + j];
-		}
+	{
+		const i = notify("loading wasm");
+		await import("../wasm-pack/client.js")
+			.then((e) => {
+				e.default();
+				notify("OK \nloaded client.wasm");
+			}).catch((e) => {
+				notify(
+					`ERR\n#failed to load client.wasm\n#does your browser support it?\n${e}`,
+					5000,
+				);
+			}).finally((_) => {
+				i.remove();
+			});
 	}
-	console.log(g);
-	const l = await post(`!/${game}`, { "Authorization": token }, "3:6-3:4");
-	console.log(l);
+
+	const updateboard = (board) => {
+		for (let i = 0; i < board.w; i++) {
+			for (let j = 0; j < board.w; j++) {
+				document.getElementById(`${i}:${j}`).innerText =
+					board.d[i * board.w + j];
+			}
+		}
+	};
+
+	const joingame = async (name) => {
+		const i = notify(`joining "${name}"`);
+		const d = await get(`${url}/!/${name}`);
+		const g = d.split("\n");
+		const token = g[0];
+		const board = {
+			d: g[1],
+			w: Math.sqrt(g[1].length),
+		};
+
+		e.innerHTML = null;
+		e.appendChild((() => {
+			const e = document.createElement("div");
+			e.innerText = "w";
+			e.id = "tl";
+			return e;
+		})());
+		for (let i = 0; i < board.w; i++) {
+			e.appendChild((() => {
+				const e = document.createElement("label");
+				e.id = `${i}:`;
+				e.innerText = i;
+				return e;
+			})());
+		}
+		for (let i = 0; i < board.w; i++) {
+			e.appendChild((() => {
+				const e = document.createElement("label");
+				e.id = `:${i}`;
+				e.innerText = i;
+				return e;
+			})());
+			for (let j = 0; j < board.w; j++) {
+				e.appendChild((() => {
+					const e = document.createElement("button");
+					e.id = `${i}:${j}`;
+					const r = document.getElementById(`:${i}`);
+					const c = document.getElementById(`${j}:`);
+					e.addEventListener("mouseover", () => {
+						r.style.color = "var(--acc)";
+						c.style.color = "var(--acc)";
+					});
+					e.addEventListener("mouseout", () => {
+						r.style.color = "";
+						c.style.color = "";
+					});
+					return e;
+				})());
+			}
+		}
+
+		updateboard(board);
+
+		notify(`OK \njoined "${name}"`);
+		i.remove();
+		return { token, board };
+	};
+
+	const { token, board } = await joingame("game");
+
+	await post(`${url}/!/game`);
+	console.log(token);
+	console.log(board);
 })();
